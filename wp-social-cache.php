6ff2a8777cf422dc1c4121ac67da16155b4509a2
Frederick Lawler <fred@fredlawl.com>
2014-07-07 10:51:58 -0500

    Added plugin information

---
+++ wp-social-cache.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: WP Social Cache
+Plugin URI: https://github.com/40Digits/WP-Social-Cache
+Description: A developer interface for accessing social media feeds.
+Version: 1.0.0
+Author: 40Digits
+Author URI: http://www.40digits.com
+License: MIT
+*/
