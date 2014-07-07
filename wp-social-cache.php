78c1578fbc7eeb40051adfd45e94d321d337a1f6
Frederick Lawler <fred@fredlawl.com>
2014-07-07 12:46:35 -0500

    Setup basic hooks etc... for testing proper beginning intializtion

--- wp-social-cache.php
+++ wp-social-cache.php
@@ -8,3 +8,33 @@ Author: 40Digits
 Author URI: http://www.40digits.com
 License: MIT
 */
+
+require_once('constants.php');
+require_once('autoload.php');
+
+
+function wpsc_initiate () {
+    do_action('wpsc_initiate');
+}
+
+function wpsc_load_admin_scripts () {
+
+}
+
+function wpsc_load_plugin_scripts () {
+
+}
+
+/*
+    Setup Actions/Hooks
+*/
+add_action('init', 'wpsc_initiate');
+add_action('admin_enqueue_scripts', 'wpsc_load_admin_scripts');
+add_action('wp_enqueue_scripts', 'wpsc_load_plugin_scripts');
+
+// Administration Menu
+// add_action('admin_menu', array('WPTAdmin', 'install'));
+
+// Plugin Install/Uninstall hooks
+// register_activation_hook(__FILE__, array('WPTeacher', 'install') );
+// register_deactivation_hook(__FILE__, array('WPTeacher', 'uninstall'));
