39e4d31a2e8da8e2cd07370cccfea65f7449c960
Frederick Lawler <fred@fredlawl.com>
2014-07-07 12:46:13 -0500

    Setup basic constants for the plugin to utalize when necessary

---
+++ constants.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+
+*/
+
+define('WPSC_BASE_PATH', untrailingslashit(dirname(__FILE__)));
+define('WPSC_LIBS_PATH', WPSC_BASE_PATH . '/libs/');
+define('WPSC_HELPERS_PATH', WPSC_BASE_PATH . '/helpers/');
+define('WPSC_ADMIN_PATH', WPSC_BASE_PATH . '/admin/');
